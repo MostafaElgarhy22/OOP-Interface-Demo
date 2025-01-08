42fe2ff7089c8e541f5e83f7887aaa3dc3bc2b9d
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-01-08 20:52:34 +0200

    Interface Example 01

--- OOP Interface/Program.cs
+++ OOP Interface/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace OOP_Interface
+﻿using OOP_Interface.Interface_Example01;
+
+namespace OOP_Interface
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-          
+            #region Interface Example 01
+            //IType reference;
+            //// Declare for refrence of type "IType" , contain Garbage Value
+            //// This Refrence 'Reference' can refer to an object of any class that implements the IType interface
+            //// CLR will Allocate 4 UnInitialized Bytes in the Stack of this refrence
+            //// ClR will Allocate 0 Bytes in the Heap of this refrence
+
+            ////  reference = //new IType(); // Error: Cannot create an instance of the interface 'IType'
+            //reference = new TypeA();
+            //reference.MyProperty = 10;
+            //reference.MyMethod(); // Hello World
+            //reference.Print(); // Default Implementation Method
+            //TypeA typeA = new TypeA();
+            //typeA.MyProperty = 10;
+            //typeA.MyMethod();
+            //typeA.Print();
+            #endregion
         }
     }
 }
